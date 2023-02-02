4be2f0a88486d09c584b9675bbde67b70726ef94
Matheus <mahserafa2016@gmail.com>
2023-02-02 15:51:50 -0300

    fix connection postgres service

--- src/main/kotlin/com/notoriousz/ibank/controller/CustomerController.kt
+++ src/main/kotlin/com/notoriousz/ibank/controller/CustomerController.kt
@@ -5,6 +5,7 @@ import com.notoriousz.ibank.repository.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,9 +16,9 @@ class CustomerController(
     fun getAllCustomers(): List<CustomerModel> {
 
         val customers = listOf<CustomerModel>(
-            CustomerModel("Math", "123"),
-            CustomerModel("Math", "123"),
-            CustomerModel("Math", "123")
+            CustomerModel(name ="Math", password = "123"),
+            CustomerModel(name ="Caio", password =  "123"),
+            CustomerModel(name ="Bois", password = "123")
         )
 
        // return customerRepository.findAll().toList()
--- src/main/kotlin/com/notoriousz/ibank/repository/model/CustomerModel.kt
+++ src/main/kotlin/com/notoriousz/ibank/repository/model/CustomerModel.kt
@@ -1,9 +1,29 @@
 package com.notoriousz.ibank.repository.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+import java.util.*
 
+
+@Entity
 data class CustomerModel(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: UUID? = UUID.randomUUID(),
+
+    @Column
     var name: String,
-    var password: String
+
+    @Column
+    var password: String,
+
+    @Column
+    var createdAt: LocalDateTime = LocalDateTime.now()
 )
 
 
